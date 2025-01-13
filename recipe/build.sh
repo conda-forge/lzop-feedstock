a35d64f5698eea9a63ffe7d336b46c9344652081
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-12 23:14:45 -0800

    enable osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./autoconf
 
 ./configure --prefix=${PREFIX}
 make -j ${CPU_COUNT}
